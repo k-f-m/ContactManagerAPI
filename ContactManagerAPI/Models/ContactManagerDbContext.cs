b901dd87fe22196f91afc673b5a832f08c9bbe51
k-f-m <55965735+k-f-m@users.noreply.github.com>
2023-11-17 02:10:15 +0100

    Configure migrations and connection string

---
+++ ContactManagerAPI/Models/ContactManagerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ContactManagerAPI.Models
+{
+    // we use the Entity Framework Core to create a session with the
+    // database for querying and storing instances of entities
+    public class ContactManagerDbContext : DbContext
+    {
+        public ContactManagerDbContext(DbContextOptions<ContactManagerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
