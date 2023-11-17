2a0e2bf3b359e37d51c233006e2cd43b7d8c9dae
k-f-m <55965735+k-f-m@users.noreply.github.com>
2023-11-17 02:45:11 +0100

    Add application startup code

---
+++ ContactManagerAPI/Program.cs
@@ -0,0 +1,31 @@
+using ContactManagerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+// Initialze WebApplicationBuilder instance and connection string.
+var builder = WebApplication.CreateBuilder(args);
+string connectionString = builder.Configuration.GetConnectionString("ContactManagerDatabase");
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ContactManagerDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
